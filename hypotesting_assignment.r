581cf4fe4e77aa64312aa80d9770e3957aa9287e
therao096 <varunrao096@gmail.com>
2020-05-08 20:50:52 +0530

    updated rcode

--- hypotesting_assignment.r
+++ hypotesting_assignment.r
@@ -28,8 +28,38 @@ summary(anova_results)
 #################################################################################################
 buyerratio <- read.csv("BuyerRatio.csv")
 View(buyerratio)
-
-
-
-
-
+attach(buyerratio)
+c1 <- buyerratio$East
+c2 <- buyerratio$West
+c3 <- buyerratio$North
+c4 <- buyerratio$South
+p1 <- data.frame(c1,c2,c3,c4)
+View(p1)
+chisq.test(p1)
+#######################################################################
+costform <- read.csv("Costomer+OrderForm.csv")
+View(costform)
+attach(costform)
+Phillippines<-as.numeric(Phillippines)
+Indonesia<-as.numeric(Indonesia)
+India<-as.numeric(India)
+Malta<-as.numeric(Malta)
+costform <- data.frame(Phillippines,Indonesia,India,Malta)
+View(costform)
+attach(costform)
+stackeddata <- stack(costform)
+View(stackeddata)
+table(stackeddata)
+chisq.test(table(stackeddata))
+###########################################
+fantaloons <- read.csv("Faltoons.csv")
+View(fantaloons)
+attach(fantaloons)
+c1 <- as.numeric(fantaloons$Weekdays)
+c2 <- as.numeric(fantaloons$Weekend)
+fantaloons <- data.frame(c1,c2)
+View(fantaloons)
+stacked <- stack(fantaloons)
+View(stacked)
+table(stacked)
+chisq.test(table(stacked))
